7026772f32b99c3fea00796bea16df1c0d4e318d
Sam. O <samrey2018@gmail.com>
2018-03-10 19:45:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,9 +52,9 @@ class Student
  def self.new_from_db(row)
    self.new(row[0], row[1], row[2])
  end
-# take first element in row make it id
+ #^^^^^ take first element in row make it id
  # take second element in row make it name
-  # take three element in row make it  grade
+  # take three element in row make it  grade ^^^#
 
  def self.find_by_name(name)
    sql = "SELECT * FROM students WHERE name = ?"
